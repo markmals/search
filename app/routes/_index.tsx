d1096704d03c53b576847282fe0182431441b09f
Mark Malstrom <mark@malstrom.me>
2023-08-15 21:53:59 -0500

    Add out-of-order streaming (defer) support

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,8 @@
 import type { LoaderArgs } from "@remix-run/node"
-import { json } from "@remix-run/node"
-import { useLoaderData, useNavigation } from "@remix-run/react"
+import { defer } from "@remix-run/node"
+import { Await, useLoaderData, useNavigation } from "@remix-run/react"
 import clsx from "clsx"
-import { useMemo } from "react"
+import { Suspense, useMemo } from "react"
 import { SearchBar } from "~/components/SearchBar"
 import { OrganicSearchResult } from "~/components/SearchResult"
 import { Spinner } from "~/components/Spinner"
@@ -13,43 +13,54 @@ export async function loader({ request }: LoaderArgs) {
     let url = new URL(request.url)
     let query = url.searchParams.get("q")
 
-    let results: OrganicResult[] = []
+    let results = Promise.resolve<OrganicResult[]>([])
 
     if (query?.length) {
-        results = await search({
+        results = search({
             source: "google",
             q: query,
             num: 40,
         }).then(res => res.organic_results)
     }
 
-    return json({ results, query })
+    return defer({ results, query })
 }
 
 export default function Index() {
-    let { results } = useLoaderData<typeof loader>()
+    let { results, query } = useLoaderData<typeof loader>()
     let navigation = useNavigation()
     let isLoading = useMemo(() => navigation.state === "loading", [navigation])
+    let isSearching = useMemo(() => !!query?.length, [query])
 
     return (
         <>
             <div
                 className={clsx(
                     "flex flex-col justify-center gap-8 px-4 py-6 sm:px-64 sm:py-8",
-                    results.length ? "h-full" : "h-screen",
+                    isSearching ? "h-full" : "h-screen",
                 )}
             >
-                {!results.length && (
+                {!isSearching && (
                     <h3 className="w-full text-center text-3xl font-bold dark:text-gray-200">
                         Search
                     </h3>
                 )}
                 <SearchBar />
-                {isLoading ? (
-                    <Spinner />
-                ) : (
-                    results.map(result => <OrganicSearchResult key={result.link} data={result} />)
-                )}
+                {/* Duplicating this fallback element sucks, but it's intended behavior in Remix 😒 */}
+                {/* See: https://remix.run/docs/en/1.19.3/guides/streaming#when-does-the-fallback-render */}
+                <Suspense fallback={<Spinner />}>
+                    <Await resolve={results}>
+                        {results =>
+                            isLoading ? (
+                                <Spinner />
+                            ) : (
+                                results.map(result => (
+                                    <OrganicSearchResult key={result.link} data={result} />
+                                ))
+                            )
+                        }
+                    </Await>
+                </Suspense>
             </div>
         </>
     )
